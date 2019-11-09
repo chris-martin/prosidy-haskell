ca8a3643fc830d00a92cead9215e4df9e0dc24f4
Chris Martin <ch.martin@gmail.com>
2019-11-09 14:24:26 -0700

    (~)

--- prosidy-abstract-syntax/src/Prosidy/AbstractSyntax.hs
+++ prosidy-abstract-syntax/src/Prosidy/AbstractSyntax.hs
@@ -8,56 +8,72 @@ import qualified Data.Map as Map
 import qualified Data.Sequence as List
 import qualified Data.Set as Set
 
-data Pro (size :: Size) (context :: Context) string where
+data Pro (size :: Size) (context :: Context) (string :: *) =
 
-    -- | A Prosidy document consists of a head and a body. The first line containing only three dashes (@---@) separates the head from the body.
-    Document :: Head string -> Body string -> Document string
+    (size ~ 'One, context ~ 'RootCtx) =>
+      Document
+        -- ^ A Prosidy document consists of a head and a body. The first line
+        -- containing only three dashes (@---@) separates the head from the body.
+        (Attrs string)
+          -- ^ The beginning of a Prosidy document is the head.
+          -- Each non-empty line of the head is an attribute.
+        (Blocks string)
+          -- ^ A Prosidy document body consists of a list of blocks.
+          -- Blocks are (typically) separated by two consecutive line breaks.
 
-    -- | Lists are important in the structure Prosidy (or of any markup language) because prose is largely linear; a document body is a list of paragraphs, and paragraphs are lists of words.
-    ProList :: List (Pro 'One context string) -> Pro size context string
+    | (size ~ 'Many) => ProList (List (Pro 'One context string))
+        -- ^ Lists are important in the structure Prosidy (or of any markup language)
+        -- because prose is largely linear; a document body is a list of paragraphs,
+        -- and paragraphs are lists of words.
 
-    -- | A block of text not wrapped in any special notation is a paragraph. A paragraph is comprised of a non-empty list of inline content.
-    Paragraph :: Inlines string -> Block string
+    | (size ~ 'One, context ~ 'BlockCtx) => Paragraph (Inlines string)
+        -- ^ A block of text not wrapped in any special notation is a paragraph.
+        -- A paragraph is comprised of a non-empty list of inline content.
 
-    -- | A block of the following form:
-    --
-    -- @
-    -- #-tagname[attrs]{inlines}
-    -- @
-    TagParagraph :: TagName string -> Attrs string -> Inlines string -> Block string
+    | (size ~ 'One, context ~ 'BlockCtx) =>
+    TagParagraph (TagName string) (Attrs string) (Inlines string)
+        -- ^ A block of the following form:
+        --
+        -- @
+        -- #-tagname[attrs]{inlines}
+        -- @
 
-    -- | A block of the following form:
-    --
-    -- @
-    -- #-tagname[attrs]:end
-    -- blocks
-    -- #:end
-    -- @
-    TagBlocks :: TagName string -> Attrs string -> Blocks string -> Block string
+    | (size ~ 'One, context ~ 'BlockCtx) =>
+    TagBlocks (TagName string) (Attrs string) (Blocks string)
+        -- ^ A block of the following form:
+        --
+        -- @
+        -- #-tagname[attrs]:end
+        -- blocks
+        -- #:end
+        -- @
 
-    -- | A block of the following form:
-    --
-    -- @
-    -- #+tagname[attrs]:end
-    -- text
-    -- #:end
-    -- @
-    TagLiteral :: TagName string -> Attrs string -> string -> Block string
+    | (size ~ 'One, context ~ 'BlockCtx) =>
+    TagLiteral (TagName string) (Attrs string) string
+        -- ^ A block of the following form:
+        --
+        -- @
+        -- #+tagname[attrs]:end
+        -- text
+        -- #:end
+        -- @
 
-    -- | A tag within a paragraph, of the following form:
-    --
-    -- @
-    -- #tagname[attrs]{inlines}
-    -- @
-    TagInline :: TagName string -> Attrs string -> Inlines string -> Inline string
+    | (size ~ 'One, context ~ 'InlineCtx) =>
+    TagInline (TagName string) (Attrs string) (Inlines string)
+        -- ^ A tag within a paragraph, of the following form:
+        --
+        -- @
+        -- #tagname[attrs]{inlines}
+        -- @
 
-    -- | Plain text
-    String :: string -> Inline string
+    | (size ~ 'One, context ~ 'InlineCtx) => String string -- ^ Plain text
 
-    -- | A line break within a paragraph. When a Prosidy document is rendered into another format, typically soft breaks are either replaced with a space character (or, in a CJK writing system, are simply removed).
-    SoftBreak :: Inline string
+    | (size ~ 'One, context ~ 'InlineCtx) => SoftBreak
+        -- ^ A line break within a paragraph. When a Prosidy document is rendered
+        -- into another format, typically soft breaks are either replaced with
+        -- a space character (or, in a CJK writing system, are simply removed).
 
-    TagName :: string -> TagName string
+    | (size ~ 'One, context ~ 'TagNameCtx) => TagName string
 
 data Size = One | Many
 
@@ -65,12 +81,6 @@ data Context = RootCtx | BlockCtx | InlineCtx | TagNameCtx
 
 -- | A Prosidy document consists of a head and a body. The first line containing only three dashes (@---@) separates the head from the body.
 type Document = Pro 'One 'RootCtx
-
--- | The beginning of a Prosidy document is the head. Each non-empty line of the head is an attribute.
-type Head = Attrs
-
--- | A Prosidy document body consists of a list of blocks. Blocks are (typically) separated by two consecutive line breaks.
-type Body = Blocks
 
 -- | A block is either a 'Paragraph' (text not wrapped in any special notation) or a tag ('TagParagraph', 'TagBlocks', or 'TagLiteral') beginning with (@#+@) or (@#-@).
 type Block = Pro 'One 'BlockCtx
