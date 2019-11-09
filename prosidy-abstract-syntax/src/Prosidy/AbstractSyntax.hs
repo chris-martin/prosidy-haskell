3681dce5cfa7d809f43ff2f2fd4c4ac47df479b3
Chris Martin <ch.martin@gmail.com>
2019-11-09 14:35:14 -0700

    remove set/map deps

--- prosidy-abstract-syntax/src/Prosidy/AbstractSyntax.hs
+++ prosidy-abstract-syntax/src/Prosidy/AbstractSyntax.hs
@@ -4,33 +4,30 @@
 
 module Prosidy.AbstractSyntax where
 
-import qualified Data.Map as Map
-import qualified Data.Set as Set
-
-data Pro (size :: Size) (context :: Context) (string :: *) (list :: * -> *) =
+data Pro (size :: Size) (context :: Context) (string :: *) (list :: * -> *) (map :: * -> * -> *) =
 
     (size ~ 'One, context ~ 'RootCtx) =>
       Document
         -- ^ A Prosidy document consists of a head and a body. The first line
         -- containing only three dashes (@---@) separates the head from the body.
-        (Attrs string)
+        (Attrs string map)
           -- ^ The beginning of a Prosidy document is the head.
           -- Each non-empty line of the head is an attribute.
-        (Blocks string list)
+        (Blocks string list map)
           -- ^ A Prosidy document body consists of a list of blocks.
           -- Blocks are (typically) separated by two consecutive line breaks.
 
-    | (size ~ 'Many) => ProList (list (Pro 'One context string list))
+    | (size ~ 'Many) => ProList (list (Pro 'One context string list map))
         -- ^ Lists are important in the structure Prosidy (or of any markup language)
         -- because prose is largely linear; a document body is a list of paragraphs,
         -- and paragraphs are lists of words.
 
-    | (size ~ 'One, context ~ 'BlockCtx) => Paragraph (Inlines string list)
+    | (size ~ 'One, context ~ 'BlockCtx) => Paragraph (Inlines string list map)
         -- ^ A block of text not wrapped in any special notation is a paragraph.
         -- A paragraph is comprised of a non-empty list of inline content.
 
     | (size ~ 'One, context ~ 'BlockCtx) =>
-    TagParagraph (TagName string) (Attrs string) (Inlines string list)
+    TagParagraph (TagName string) (Attrs string map) (Inlines string list map)
         -- ^ A block of the following form:
         --
         -- @
@@ -38,7 +35,7 @@ data Pro (size :: Size) (context :: Context) (string :: *) (list :: * -> *) =
         -- @
 
     | (size ~ 'One, context ~ 'BlockCtx) =>
-    TagBlocks (TagName string) (Attrs string) (Blocks string list)
+    TagBlocks (TagName string) (Attrs string map) (Blocks string list map)
         -- ^ A block of the following form:
         --
         -- @
@@ -48,7 +45,7 @@ data Pro (size :: Size) (context :: Context) (string :: *) (list :: * -> *) =
         -- @
 
     | (size ~ 'One, context ~ 'BlockCtx) =>
-    TagLiteral (TagName string) (Attrs string) string
+    TagLiteral (TagName string) (Attrs string map) string
         -- ^ A block of the following form:
         --
         -- @
@@ -58,7 +55,7 @@ data Pro (size :: Size) (context :: Context) (string :: *) (list :: * -> *) =
         -- @
 
     | (size ~ 'One, context ~ 'InlineCtx) =>
-    TagInline (TagName string) (Attrs string) (Inlines string list)
+    TagInline (TagName string) (Attrs string map) (Inlines string list map)
         -- ^ A tag within a paragraph, of the following form:
         --
         -- @
@@ -90,12 +87,6 @@ type Inline = Pro 'One 'InlineCtx
 
 type Inlines = Pro 'Many 'InlineCtx
 
-type TagName string = forall list. Pro 'One 'TagNameCtx string list
+type TagName string = forall list map. Pro 'One 'TagNameCtx string list map
 
-data Attrs string = Attrs (Set string) (Map string string)
-
--- | A map from type @key@ to type @value@, sorted by key. See "Data.Map".
-type Map key value = Map.Map key value
-
--- | A sorted collection of distinct @element@s. See "Data.Set".
-type Set element = Set.Set element
+data Attrs string map = Attrs (map string ()) (map string string)
