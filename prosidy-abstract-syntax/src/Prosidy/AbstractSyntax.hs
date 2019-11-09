9730228616327bc88afafbb1f273aad94692898e
Chris Martin <ch.martin@gmail.com>
2019-11-09 13:13:33 -0700

    remove OneOrMore

--- prosidy-abstract-syntax/src/Prosidy/AbstractSyntax.hs
+++ prosidy-abstract-syntax/src/Prosidy/AbstractSyntax.hs
@@ -16,10 +16,10 @@ data Pro (size :: Size) (context :: Context) where
     Document :: Head -> Body -> Document
 
     -- | Lists are important in the structure Prosidy (or of any markup language) because prose is largely linear; a document body is a list of paragraphs, and paragraphs are lists of words.
-    ProList :: List size (Pro 'One context) -> Pro size context
+    ProList :: List (Pro 'One context) -> Pro size context
 
     -- | A block of text not wrapped in any special notation is a paragraph. A paragraph is comprised of a non-empty list of inline content.
-    Paragraph :: Inlines1 -> Block
+    Paragraph :: Inlines -> Block
 
     -- | A block of the following form:
     --
@@ -54,12 +54,12 @@ data Pro (size :: Size) (context :: Context) where
     TagInline :: TagName -> Attrs -> Inlines -> Inline
 
     -- | Plain text
-    Text :: Chars1 -> Inline
+    Text :: Chars -> Inline
 
     -- | A line break within a paragraph. When a Prosidy document is rendered into another format, typically soft breaks are either replaced with a space character (or, in a CJK writing system, are simply removed).
     SoftBreak :: Inline
 
-data Size = One | AnyNumber | OneOrMore
+data Size = One | Many
 
 data Context = Root | Block | Inline
 
@@ -75,44 +75,33 @@ type Body = Blocks
 -- | A block is either a 'Paragraph' (text not wrapped in any special notation) or a tag ('TagParagraph', 'TagBlocks', or 'TagLiteral') beginning with (@#+@) or (@#-@).
 type Block = Pro 'One 'Block
 
-type Blocks = Pro 'AnyNumber 'Block
-type Blocks1 = Pro 'OneOrMore 'Block
+type Blocks = Pro 'Many 'Block
 type Inline = Pro 'One 'Inline
-type Inlines = Pro 'AnyNumber 'Inline
-type Inlines1 = Pro 'OneOrMore 'Inline
+type Inlines = Pro 'Many 'Inline
 
-newtype TagName = TagName Chars1
+newtype TagName = TagName Chars
 data Attrs = Attrs (Set Flag) (Map FieldName FieldValue)
-newtype Flag = Flag Chars1
-newtype FieldName = FieldName Chars1
+newtype Flag = Flag Chars
+newtype FieldName = FieldName Chars
 newtype FieldValue = FieldValue Chars
 
--- | A finite sequence of elements, with the parameter @size@ constraining how many elements are in the sequence, and the parameter @element@ indicating the type of the elements. See "Data.Sequence".
-data List (size :: Size) (element :: *) where
+-- | A finite sequence of elements, with the parameter @size@ constraining how many elements are in the sequence, and the parameter @element@ indicating the type of the elements.
+data List (element :: *) where
 
-    -- | A possibly-empty list.
-    List0 :: List.Seq element -> List 'AnyNumber element
+    -- | A finger tree. See "Data.Sequence".
+    List0 :: List.Seq element -> List element
 
-    -- | A non-empty list. This constructor is designated "unsafe" because it is the user's responsibility to ensure that this list is actually non-empty.
-    List1_Unsafe :: List.Seq element -> List 'OneOrMore element
-
-    -- | A possibly-empty packed list of characters. See "Data.Text".
+    -- | A packed list of characters. See "Data.Text".
     Text0 :: Text.Text -> Chars
-
-    -- | A non-empty packed list of characters. This constructor is designated "unsafe" because it is the user's responsibility to ensure that this list is actually non-empty.
-    Text1_Unsafe :: Text.Text -> Chars1
 
 -- | A single character.
 type Char = Char.Char
 
--- | A possibly-empty list of characters.
-type Chars = List 'AnyNumber Char
+-- | A list of characters.
+type Chars = List Char
 
--- | A non-empty list of characters.
-type Chars1 = List 'OneOrMore Char
-
--- | A possibly-empty map from type @key@ to type @value@, sorted by key. See "Data.Map".
+-- | A map from type @key@ to type @value@, sorted by key. See "Data.Map".
 type Map key value = Map.Map key value
 
--- | A possibly-empty sorted collection of distinct @element@s. See "Data.Set".
+-- | A sorted collection of distinct @element@s. See "Data.Set".
 type Set element = Set.Set element
