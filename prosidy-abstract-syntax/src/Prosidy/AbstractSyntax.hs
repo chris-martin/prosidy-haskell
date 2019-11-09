426e299e30e5c1d6fdf0cffd8f2e0054c43f579a
Chris Martin <ch.martin@gmail.com>
2019-11-09 16:03:57 -0700

    line wrap

--- prosidy-abstract-syntax/src/Prosidy/AbstractSyntax.hs
+++ prosidy-abstract-syntax/src/Prosidy/AbstractSyntax.hs
@@ -1,6 +1,6 @@
 {-# OPTIONS_GHC -Wall #-}
 
-{-# LANGUAGE DataKinds, KindSignatures, GADTs, NoImplicitPrelude, RankNTypes, StandaloneDeriving #-}
+{-# LANGUAGE DataKinds, KindSignatures, GADTs, NoImplicitPrelude #-}
 
 module Prosidy.AbstractSyntax where
 
@@ -16,27 +16,32 @@ data Pro
       ->
         Pro string list attrs 'Many 'Block
           -- ^ A Prosidy document body consists of a list of blocks.
-          -- Blocks are (typically) separated by two consecutive line breaks.
+          -- Blocks are (typically) separated by two consecutive line
+          -- breaks.
       ->
         Pro string list attrs 'One 'Root
-          -- ^ A Prosidy document consists of a head and a body. The first line
-          -- containing only three dashes (@---@) separates the head from the body.
+          -- ^ A Prosidy document consists of a head and a body. The
+          -- first line containing only three dashes (@---@) separates
+          -- the head from the body.
 
     List ::
         list (Pro string list attrs 'One context)
           -- ^ e.g. a list of blocks or a list of inlines
       ->
         Pro string list attrs 'Many context
-          -- ^ Lists are important in the structure Prosidy (or of any markup
-          -- language) because prose is largely linear; a document body is a
-          -- list of paragraphs, and paragraphs are lists of words.
+          -- ^ Lists are important in the structure Prosidy (or of
+          -- any markup language) because prose is largely linear;
+          -- a document body is a list of paragraphs, and paragraphs
+          -- are lists of words.
 
     Paragraph ::
         Pro string list attrs 'Many 'Inline
-          -- ^ A list of inline elements (plain text, inline tags, soft breaks).
+          -- ^ A list of inline elements (plain text, inline tags,
+          -- soft breaks).
       ->
         Pro string list attrs 'One 'Block
-          -- ^ A block of text not wrapped in any special notation is a paragraph.
+          -- ^ A block of text not wrapped in any special notation
+          -- is a paragraph.
 
     TagParagraph ::
         Tag string
@@ -119,16 +124,19 @@ data Pro
 
     SoftBreak ::
         Pro string list attrs 'One 'Inline
-          -- ^ A line break within a paragraph. When a Prosidy document is rendered
-          -- into another format, typically soft breaks are either replaced with
-          -- a space character (or, in a CJK writing system, are simply removed).
+          -- ^ A line break within a paragraph. When a Prosidy document is
+          -- rendered into another format, typically soft breaks are either
+          -- replaced with a space character (or, in a CJK writing system,
+          -- are simply removed).
 
 data Size = One | Many
 
 data Context =
     Root
   | Block
-      -- ^ A block is either a 'Paragraph' (text not wrapped in any special notation) or a tag ('TagParagraph', 'TagBlocks', or 'TagLiteral') beginning with (@#+@) or (@#-@).
+      -- ^ A block is either a 'Paragraph' (text not wrapped in any special
+      -- notation) or a tag ('TagParagraph', 'TagBlocks', or 'TagLiteral')
+      -- beginning with (@#+@) or (@#-@).
   | Inline
 
 newtype Tag string = Tag string
