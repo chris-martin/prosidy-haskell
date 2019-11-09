be21eca635f33c8a6ba2662089b6a3ae4e2182ec
Chris Martin <ch.martin@gmail.com>
2019-11-09 13:26:15 -0700

    remove Text import

--- prosidy-abstract-syntax/src/Prosidy/AbstractSyntax.hs
+++ prosidy-abstract-syntax/src/Prosidy/AbstractSyntax.hs
@@ -4,29 +4,27 @@
 
 module Prosidy.AbstractSyntax where
 
-import qualified Data.Char as Char
 import qualified Data.Map as Map
 import qualified Data.Sequence as List
 import qualified Data.Set as Set
-import qualified Data.Text as Text
 
-data Pro (size :: Size) (context :: Context) where
+data Pro (size :: Size) (context :: Context) string where
 
     -- | A Prosidy document consists of a head and a body. The first line containing only three dashes (@---@) separates the head from the body.
-    Document :: Head -> Body -> Document
+    Document :: Head string -> Body string -> Document string
 
     -- | Lists are important in the structure Prosidy (or of any markup language) because prose is largely linear; a document body is a list of paragraphs, and paragraphs are lists of words.
-    ProList :: List (Pro 'One context) -> Pro size context
+    ProList :: List (Pro 'One context string) -> Pro size context string
 
     -- | A block of text not wrapped in any special notation is a paragraph. A paragraph is comprised of a non-empty list of inline content.
-    Paragraph :: Inlines -> Block
+    Paragraph :: Inlines string -> Block string
 
     -- | A block of the following form:
     --
     -- @
     -- #-tagname[attrs]{inlines}
     -- @
-    TagParagraph :: TagName -> Attrs -> Inlines -> Block
+    TagParagraph :: TagName string -> Attrs string -> Inlines string -> Block string
 
     -- | A block of the following form:
     --
@@ -35,7 +33,7 @@ data Pro (size :: Size) (context :: Context) where
     -- blocks
     -- #:end
     -- @
-    TagBlocks :: TagName -> Attrs -> Blocks -> Block
+    TagBlocks :: TagName string -> Attrs string -> Blocks string -> Block string
 
     -- | A block of the following form:
     --
@@ -44,20 +42,20 @@ data Pro (size :: Size) (context :: Context) where
     -- text
     -- #:end
     -- @
-    TagLiteral :: TagName -> Attrs -> Chars -> Block
+    TagLiteral :: TagName string -> Attrs string -> string -> Block string
 
     -- | A tag within a paragraph, of the following form:
     --
     -- @
     -- #tagname[attrs]{inlines}
     -- @
-    TagInline :: TagName -> Attrs -> Inlines -> Inline
+    TagInline :: TagName string -> Attrs string -> Inlines string -> Inline string
 
     -- | Plain text
-    Text :: Chars -> Inline
+    String :: string -> Inline string
 
     -- | A line break within a paragraph. When a Prosidy document is rendered into another format, typically soft breaks are either replaced with a space character (or, in a CJK writing system, are simply removed).
-    SoftBreak :: Inline
+    SoftBreak :: Inline string
 
 data Size = One | Many
 
@@ -79,26 +77,14 @@ type Blocks = Pro 'Many 'Block
 type Inline = Pro 'One 'Inline
 type Inlines = Pro 'Many 'Inline
 
-newtype TagName = TagName Chars
-data Attrs = Attrs (Set Flag) (Map FieldName FieldValue)
-newtype Flag = Flag Chars
-newtype FieldName = FieldName Chars
-newtype FieldValue = FieldValue Chars
+newtype TagName string = TagName string
+data Attrs string = Attrs (Set string) (Map string string)
 
 -- | A finite sequence of elements, with the parameter @size@ constraining how many elements are in the sequence, and the parameter @element@ indicating the type of the elements.
 data List (element :: *) where
 
     -- | A finger tree. See "Data.Sequence".
     List0 :: List.Seq element -> List element
-
-    -- | A packed list of characters. See "Data.Text".
-    Text0 :: Text.Text -> Chars
-
--- | A single character.
-type Char = Char.Char
-
--- | A list of characters.
-type Chars = List Char
 
 -- | A map from type @key@ to type @value@, sorted by key. See "Data.Map".
 type Map key value = Map.Map key value
